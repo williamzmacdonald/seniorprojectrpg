51b329b8d462ea69c0f4da24c95625fb4716665f
williamzmacdonald <33135172+williamzmacdonald@users.noreply.github.com>
2018-03-01 11:25:06 -0600

    fixed login
    fixed route forlogin form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
                 <div class="panel-heading">Login</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ url('rooms') }}">
+                    <form class="form-horizontal" method="POST" action="{{ url('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
