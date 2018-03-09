2902f38830e0090a1f943757981adf1bab5ff763
williamzmacdonald <33135172+williamzmacdonald@users.noreply.github.com>
2018-03-09 12:03:16 -0600

    Added authentication to be allowed into rooms view

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -20,6 +20,10 @@ function randomURL($URLlength = 8) {
 }
 class RoomsController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
     public function index()
 	{
 		$user = \Auth::user();
