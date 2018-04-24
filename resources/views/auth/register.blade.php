fefb408ed37ce21c0c583717b46aeecaca479d5c
Erin Reyes <erin.reyes01@utrgv.edu>
2018-04-24 10:55:48 -0500

    fixed panels

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,14 +6,18 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading clearfix">
+                    <h4 class="panel-title pull-left" style="padding-top: 7.5 px">Register</h4>
+                </div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-md-4 control-label">
+                                <h5 style="margin-top: 5px;">Name</h5>
+                            </label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
@@ -27,7 +31,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">
+                                <h5 style="margin-top: 5px;">E-Mail Address</h5>
+                            </label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
@@ -41,7 +47,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">
+                                <h5 style="margin-top: 5px;">Password</h5>
+                            </label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -55,7 +63,9 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <label for="password-confirm" class="col-md-4 control-label">
+                                <h5 style="margin-top: 5px;">Confirm Password</h5>
+                            </label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
--- resources/views/rooms/create.blade.php
+++ resources/views/rooms/create.blade.php
@@ -5,20 +5,26 @@
     <div class="container">
         <div class ="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
-                <div class="panel-heading">Create Your Room</div>
+                <div class="panel-heading clearfix">
+                    <h4 class="panel-title pull-left" style="padding-top: 7.5 px">Create Room</h4>
+                </div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ url('rooms') }}">
                         {{ csrf_field() }}
                         <div class="form-group">
-                            <label for="title" class="col-md-4 control-label">Title</label>
+                            <label for="title" class="col-md-4 control-label">
+                                <h5 style="margin-top: 5px;">Title</h5>
+                            </label>
                             <div class="col-md-6">
                                 <input type="text" class="form-control" id="title" name ="title">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label for="description" class="col-md-4 control-label">Description</label>
+                            <label for="description" class="col-md-4 control-label">
+                                <h5 style="margin-top: 5px;">Description</h5>
+                            </label>
                             <div class="col-md-6">
                                 <input type="text" class="form-control" id="description" name="description">
                             </div>
